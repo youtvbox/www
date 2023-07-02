4fd9a12c2060a9d66490a3fac2eb9d36b3681e51
zouplus <zouplus@zou.plus>
2023-07-02 22:51:01 +0800

    修改首页

---
+++ config.js
@@ -0,0 +1,12 @@
+window.config = {
+    link: [
+        {
+            name: '1.01版本最新下载地址',
+            path:'http://www.baidu.com'
+        },
+        {
+            name: '1.02版本最新下载地址',
+            path:'http://www.baidu.com'
+        }
+    ]
+}
