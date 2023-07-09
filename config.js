d8c629988ca26e8d458ebfcc25183a09c956dc6c
zouplus <zouplus@zou.plus>
2023-07-09 17:35:02 +0800

    修改官网

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ window.config = {
     link: [
         {
             name: '1.03TV版本下载地址',
-            path:'http://www.baidu.com'
+            path:'https://develope.lanzoug.com/file/?CG4FOwg5Dj9RWFdvBjNXOwM8VW1e5wK4Ue4A41CxBrYA6FSkWYIGYwR8VjBQNwAvUWoCdwQwAycDYlU6UWFTNwhXBTsIMQ5nUTZXMwZnV2ADaFVnXjcCNlFkACBQagZ0AD1UMVkzBmEEY1YyUDUAM1E8AiEEKwNxAzlVYVE9U2AIOwV9CGUObVEoVzEGa1d5A2FVMl42AjNRZwBkUG0GMAA2VGBZNwZrBGdWN1BmAGRRbwI2BGMDZQNmVTFROlMwCGkFYwhsDmJRZldgBmFXZAN3VTFecQJrUXAAc1B/BmIAclRqWWAGbwRhVjRQMgA2UTsCMgRpAycDcFU6UWBTNwhuBW8IZA5sUT9XOQZqV28Db1VhXjkCNFF4AChQKgZhAGxUdFk5BmIEdFZ1UHQAdlE2AjYEbQM5AzxVa1E0U2YIMAVmCGUOfFFyV2gGIldrA2tVYl4zAi5RZwA+UDsGKQAyVDBZPQZ8BGRWNQ=='
         },
         {
-            name: '1.03TV 版 蓝奏下载地址',
-            path:'http://www.baidu.com'
+            name: '1.03TV 版 蓝奏下载地址（密码：3k1o)',
+            path:'https://wwaz.lanzoum.com/iHe4v11re6hc'
         }
     ]
 }
