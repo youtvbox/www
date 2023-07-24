fbe4177e8c858e5d23b731e531ef3490bc1d8b31
zouplus <zouplus@zou.plus>
2023-07-24 10:56:31 +0800

    update download url

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ window.config = {
     link: [
         {
             name: '1.03TV版本下载地址',
-            path:'https://gitee.com/goodfree/utv/releases/download/blob/游影视【蜂蜜】%20TV.apk.apk'
+            path:'https://gitee.com/goodfree/utv/releases/download/blob/游影视【蜂蜜】 TV.apk.apk'
         },
         {
             name: '手机版',
-            path:'https://gitee.com/goodfree/utv/releases/download/blob/游影视【蜂蜜】%20手机.apk'
+            path:'https://gitee.com/goodfree/utv/releases/download/blob/游影视【蜂蜜】 手机.apk'
         }
     ]
 }
