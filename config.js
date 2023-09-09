f19ef5a37d78d2c1b8de6af39587ab6a32057493
zouplus <zouplus@zou.plus>
2023-09-09 17:43:32 +0800

    add 1004

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.config = {
     link: [
         {
-            name: '1.03TV版本下载地址',
-            path:'https://gitee.com/goodfree/utv/releases/download/blob/游影视【蜂蜜】 TV.apk'
+            name: '1.04TV版本下载地址',
+            path:'https://gitee.com/meetutv/utv/releases/download/1004/1004.apk'
         },
         {
             name: '手机版',
